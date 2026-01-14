58465bbff99391aa9a82b60a12c7148fab535544
Sainath Shivaji Mitalakar <94965381+sainathmitalakar@users.noreply.github.com>
2026-01-14 10:16:32 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,25 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'org-setup' // 🔴 MUST MATCH YOUR REPO NAME
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/org-setup' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/org-setup' : '',
+
   experimental: {
     mdxRs: false,
-  }
+  },
 }
 
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
 })
 
 module.exports = withMDX(nextConfig)
